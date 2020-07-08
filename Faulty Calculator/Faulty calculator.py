378ecc42e33cdfc29573023fb6bf4eebd7ee3e3b
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-07-08 19:00:08 +0530

    Update Faulty calculator.py

--- Faulty Calculator/Faulty calculator.py
+++ Faulty Calculator/Faulty calculator.py
@@ -1,8 +1,8 @@
-# a calculator that solves all the problems correctly but not these problems.
-# 45 * 3 = 155, 56 + 9 = 77, 56 / 6 = 4
-opp = input("Enter the operator from + * / \n")
-num1 = int(input("Enter the Operand 1"))
-num2 = int(input("Enter the Operand 2"))
+# A calculator that solves all the problems correctly but not these problems.
+# 45 * 3 = 155, 56 + 9 = 77, 56 / 6 = 4.
+opp = input("Enter the operator from + * / - % \n")
+num1 = int(input("Enter the Operand 1\n"))
+num2 = int(input("Enter the Operand 2\n"))
 if opp == "*":
     if num1 == 45 and num2 == 3:
         print("Value is 155")
@@ -18,4 +18,7 @@ elif opp == "/":
         print("Value is 4")
     else:
         print("value is ",num1/num2)
-
+elif opp == "-":
+    print("Value is", num1-num2)
+elif opp == "%":
+    print("Value is", num1%num2)
