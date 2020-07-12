c0896763b7aeee5be320f2ddfd95b05c8384465c
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-07-12 07:19:37 -0700

    Create Word_and_its_meaning.py

---
+++ Dictionary/Word_and_its_meaning.py
@@ -0,0 +1,11 @@
+# Make a dictionary and take input from the user and display the meaning of the word.
+# Input words are mutable, immutable, wise, manmade, which each have their seperate meanings coded in the dictionary labeled as Dict1.
+
+Dict1 = {"Mutable":"It can be changed",
+         "Immutable":"It cannot be changed",
+         "Wise":"The person who is morally correct",
+         "Manmade":"The objects that were not readily available in nature."}
+print("Search for the meanings of the following words:\n",
+      "1. Immutable\n","2. Mutable\n","3. Wise\n","4. Manmade\n")
+word = input()
+print(Dict1[word])
