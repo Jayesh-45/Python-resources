547dd1ce62f1e47112b0a6b9efa641e8bbb63864
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-07-14 08:12:46 -0700

    Update Palindrom_String&number.py

--- Palindrome/Palindrom_String&number.py
+++ Palindrome/Palindrom_String&number.py
@@ -1,25 +1,23 @@
 
 #### pallindrome string
 
-      string=input(("Enter a string:"))
-
-      if(string==string[::-1]):
-
-      print("The string is a palindrome")
-
+      string = input(("Enter a string: \n"))
+      if(string == string[::-1]): 
+            print("The string is a palindrome")
+      
       else:
-     print("Not a palindrome")
+           print("Not a palindrome")
 
 #### pallindrome number
 
-     num=int(input("Enter a number:"))
-     temp=num
-     rev=0
+     num = int(input("Enter a number: \n"))
+     temp = num
+     rev = 0
      while(num>0):
-       dig=num%10
-       rev=rev*10+dig
-       num=num//10
-     if(temp==rev):
+       dig = num % 10
+       rev = rev * 10 + dig
+       num = num//10
+     if(temp == rev):
         print("The number is palindrome!")
     else:
        print("Not a palindrome!")
