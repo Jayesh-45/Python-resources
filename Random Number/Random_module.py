6e77161257dfe50f010d0f6c7c86af5b5d3f6c0a
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-07-27 06:23:24 -0700

    Create Random_module.py

---
+++ Random Number/Random_module.py
@@ -0,0 +1,9 @@
+# Here we are importing the "random" module in Python.
+# To find a random integer in a perticular range.
+
+import random
+# This function generates a random integer from 0 to 50.
+var1 = random.randint(0, 50)
+
+#Here we print the random integer.
+print(var1)
