e9aba8392a6b45343e101d451ce3bc12c6883f3c
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-07-28 05:36:37 -0700

    Update Random_module.py

--- Random Number/Random_module.py
+++ Random Number/Random_module.py
@@ -3,7 +3,11 @@
 
 import random
 # This function generates a random integer from 0 to 50.
-var1 = random.randint(0, 50)
 
-#Here we print the random integer.
+var1 = random.randint(0, 50)
 print(var1)
+
+# Function to generate a random integer number within a range.
+
+var2 = random.randrange(3,30, 3)
+print(var2)
