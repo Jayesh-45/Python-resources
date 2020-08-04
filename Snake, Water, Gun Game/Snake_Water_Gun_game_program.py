f764f1c733cf2de054749b88a2eb1df56928af3c
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-08-05 00:26:42 +0530

    Create Snake_Water_Gun_game_program.py

---
+++ Snake, Water, Gun Game/Snake_Water_Gun_game_program.py
@@ -0,0 +1,15 @@
+#This is a Snake, Water and Gun Game.
+
+import random
+list = ['s', 'w', 'g']# this is a list for snake, water and gun..
+
+# We have 10 chances.
+chances = 10
+no_of_chance = 0
+comp_points = 0
+your_points = 0
+
+print("This is Your Snake, Water and Gun Game..")
+print("Chose 's' for Snake, 'w' for Water, 'g' for Gun..")
+
+# This is the Game
