f765746d32b244984f14c39fdb9643c05c228dab
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-08-06 09:33:26 +0530

    Update Snake_Water_Gun_game_program.py

--- Snake, Water, Gun Game/Snake_Water_Gun_game_program.py
+++ Snake, Water, Gun Game/Snake_Water_Gun_game_program.py
@@ -13,3 +13,25 @@ print("This is Your Snake, Water and Gun Game..")
 print("Chose 's' for Snake, 'w' for Water, 'g' for Gun..")
 
 # This is the Game
+
+while no_of_chance < chances:
+    # We input the choice of the user here.
+    _input = input('Snake, Water or Gun:\n')
+    #Here we select the random choice of the computer from the list.
+    _random = random.choice(list)
+
+    if _input == _random:
+        print("Its a Tie. 0 points to each..")
+
+    # When user inputs S.
+    elif _input == "s" and _random == "w":
+        your_points = your_points + 1
+        print(f"Your input was {_input} and the Computers choice was {_random}. ")
+        print("You win 1 point..")
+        print(f"Your points are {your_points} and the Computer's points are {comp_points}. ")
+
+    elif _input == "s" and _random == "g":
+        comp_points = comp_points + 1
+        print(f"Your input was {_input} and the Computers choice was {_random}. ")
+        print("Comp wins 1 point..")
+        print(f"Your points are {your_points} and the Computer's points are {comp_points}. ")
