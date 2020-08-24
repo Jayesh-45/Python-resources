34ea1ad71bc38f770326d1fc590aad7f2abc0dd0
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-08-24 12:38:49 +0530

    Update Armstrong_Number.py

--- Armstrong Number/Armstrong_Number.py
+++ Armstrong Number/Armstrong_Number.py
@@ -1,19 +1,19 @@
 # This is the program for Armstrong number.
 
 print("This is the program for Armstrong number..")
-num = int(input("Enter the number you want to check:\n"))
+number = int(input("Enter the number you want to check:\n"))
 
 sum = 0
-temp = num
+temp = number
 
 while temp > 0:
     digit = temp % 10
     sum = sum +  digit * digit * digit
     temp = temp// 10
 
-if num == sum:
-    print(num,"is a Armstrong Number..")
+if number == sum:
+    print(number,"is a Armstrong Number..")
 
 else:
-    print(num,"is not a Armstrong Number..")
+    print(number,"is not a Armstrong Number..")
 
