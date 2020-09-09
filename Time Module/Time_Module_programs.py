7bf1a4acf0334e03c00e3309fb86b7d414674642
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2020-09-09 11:56:23 +0530

    Update Time_Module_programs.py

--- Time Module/Time_Module_programs.py
+++ Time Module/Time_Module_programs.py
@@ -9,3 +9,11 @@ initial = time.time()
 for i in range(45):
      print("Hello..")
 print("The execution time was", time.time() - initial, "Seconds.")
+
+initial2 = time.time()
+k = 0
+while(k<10):
+    print("Hello")
+    time.sleep(1)
+    k = k + 1
+print("The execution time was", time.time() - initial2, "Seconds.")
